b68ef89e8b5c91ca3e539dd48f3e29b731eb89e9
Daniel <daniel.andres.ramos.v@gmail.com>
2021-11-17 12:40:01 -0300

    finalCommit

--- src/App/index.js
+++ src/App/index.js
@@ -1,31 +1,67 @@
-import React, { Component } from 'react'
-import Input from '../Input/index'
-import logo from '../resources/logo512.png'
+import React, { useState } from "react";
+import { Input } from "../Input/index";
+import logo from "../resources/logo512.png";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      inputValue: ''
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       inputValue: "",
+//     };
+//   }
+
+//   onChangeInputValue = (value) => this.setState({ inputValue: value });
+
+//   render() {
+//     const { inputValue } = this.state;
+
+//     return (
+//       <>
+//         <div>
+//           <Input
+//             inputValue={inputValue}
+//             onChangeInputValue={this.onChangeInputValue}
+//           />
+//         </div>
+//         <img src={logo} alt={"logo"} />
+//       </>
+//     );
+//   }
+// }
+
+export const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [names, setNames] = useState([]);
+  const [search, setSearch] = useState([]);
+
+  const onChangeInputValue = (value) => {
+    setInputValue(value);
+
+    if (value !== "") {
+      setSearch(
+        names.filter((x) => {
+          return x.completeName.toLowerCase().includes(value.toLowerCase());
+        })
+      );
+    } else {
+      setSearch([]);
     }
-  }
-
-  onChangeInputValue = (value) =>
-    this.setState({ inputValue: value })
-
-  render() {
-    const { inputValue } = this.state
-
-    return (
-      <>
-        <div>
-          <Input
-            inputValue={inputValue}
-            onChangeInputValue={this.onChangeInputValue}
-          />
-        </div>
-        <img src={logo} alt={'logo'} />
-      </>
-    )
-  }
-}
+  };
+  return (
+    <>
+      <div>
+        <Input
+          inputValue={inputValue}
+          onChangeInputValue={onChangeInputValue}
+          setNames={setNames}
+        />
+      </div>
+      {!!search && search.map((s) => <div key={s.id}>{s.completeName}</div>)}
+      <img
+        src={logo}
+        alt={"logo"}
+        style={{ position: "absolute", zIndex: "-1", top: "10px" }}
+      />
+    </>
+  );
+};
--- src/Input/index.js
+++ src/Input/index.js
@@ -1,38 +1,96 @@
-import React, { Component } from 'react'
-import { getFakeData1, getFakeData2 } from '../fakeRequest'
+import React, { useEffect, useState } from "react";
+import { getFakeData1, getFakeData2 } from "../fakeRequest";
 
-export default class Input extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      inputValueInComponent: ''
+// export default class Input extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       inputValueInComponent: "",
+//     };
+//   }
+
+//   async componentDidMount() {
+//     const firstNameWithId = await getFakeData1();
+//     const lastNameWithId = await getFakeData2();
+
+//     console.log({ firstNameWithId, lastNameWithId });
+//   }
+
+//   componentDidUpdate(prevProps) {
+//     if (prevProps.inputValue !== this.props.inputValue) {
+//       this.setState({
+//         inputValueInComponent: this.props.inputValue,
+//       });
+//     }
+//   }
+
+//   render() {
+//     const { inputValueInComponent } = this.state;
+//     const { onChangeInputValue } = this.props;
+
+//     return (
+//       <input
+//         onChange={(e) => onChangeInputValue(e.target.value)}
+//         value={inputValueInComponent}
+//       />
+//     );
+//   }
+// }
+
+// SEPARACION HOOKS
+
+export const Input = ({ onChangeInputValue, inputValue, setNames }) => {
+  const [firstNameWithId, setFirstNameWithId] = useState([]);
+  const [lastNameWithId, setLastNameWithId] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+
+  const getFakeLastNamee = async () => {
+    setLastNameWithId(await getFakeData2());
+  };
+  const getFakeFirstName = async () => {
+    setFirstNameWithId(await getFakeData1());
+  };
+
+  useEffect(() => {
+    if (loading) {
+      getFakeLastNamee();
+      getFakeFirstName();
     }
-  }
+  }, [loading]);
 
-  async componentDidMount() {
-    const firstNameWithId = await getFakeData1()
-    const lastNameWithId = await getFakeData2()
+  useEffect(() => {
+    if (firstNameWithId.length > 0 && lastNameWithId.length > 0) {
+      console.log({ firstNameWithId, lastNameWithId });
+      const completeName = firstNameWithId
+        .map((first) => {
+          const found = lastNameWithId.find((x) => x.id === first.id);
+          if (!!found) {
+            return {
+              id: first.id,
+              completeName: first.firstName + " " + found.lastName,
+            };
+          } else {
+            return null;
+          }
+        })
+        .filter((z) => z !== null);
+      setNames(completeName);
 
-    console.log({firstNameWithId, lastNameWithId})
-  }
-
-  componentDidUpdate(prevProps) {
-    if(prevProps.inputValue !== this.props.inputValue) {
-      this.setState({
-        inputValueInComponent: this.props.inputValue
-      })
+      setLoading(false);
     }
-  }
+  }, [firstNameWithId, lastNameWithId, setNames]);
 
-  render() {
-    const { inputValueInComponent } = this.state
-    const { onChangeInputValue } = this.props
-
-    return (
-      <input
-        onChange={e => onChangeInputValue(e.target.value)}
-        value={inputValueInComponent}
-      />
-    )
-  }
-}
+  return (
+    <>
+      {!loading ? (
+        <input
+          onChange={(e) => onChangeInputValue(e.target.value)}
+          value={inputValue}
+        />
+      ) : (
+        <div>Cargando...</div>
+      )}
+    </>
+  );
+};
